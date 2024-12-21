1b90aac7f1d8bf71b8db4c44afcf7a79a420c289
ANDREWIDEAPAD\03dre <pattona@southern.edu>
2024-12-21 15:54:59 -0500

    Added Background Connection Updates. Added Logging. Simplied Client.

--- client.py
+++ client.py
@@ -1,109 +1,77 @@
-from .consts import IN, BAD, CLEAR, BG
+from .consts import IN, BAD, CLEAR, BG, CHAT
 from .sockets import ClientSocket
-from .utils import wclear, wprint, wread, background_process
-import curses
-from curses import window
-from time import sleep
+from abc import ABC, abstractmethod
+import asyncio
+from threading import Thread
+import select
+from _socket import MSG_PEEK
+from time import sleep, asctime
+from random import randint
 
-class Client:
-    def __init__(self, window: window = None, host: str = "", port: int = 0):
+LOG_PATH = "client_debug_log.txt"
+ID = randint(1, 100)
+def add_to_log(message: str):
+    with open(LOG_PATH, 'a') as log:
+        log.write(f"{asctime()} | PID {ID}: {message}\n")
+
+class BaseClient(ABC):
+    def __init__(self):
+        super().__init__()
         self.connected = False
-        if not window:
-            window = curses.initscr()
-        self.main_window = window
-        if host and port:
-            self.connect()
-        
-
-    def build_window(self):
-        self.lines, self.cols = self.main_window.getmaxyx()
-        self.beg_y, self.beg_x = self.main_window.getbegyx()
-        self.main_window.noutrefresh()
-        self.game_area = self.make_game_area()
-        self.message_box = self.make_message_box()
-        curses.doupdate()
-            
-    def make_game_area(self):
-        outline = curses.newwin(self.lines - 3, self.cols, 0 + self.beg_y, 0 + self.beg_x)
-        # outline.box()
-        game_area = curses.newwin(self.lines - 5, self.cols - 4, 1 + self.beg_y, 2 + self.beg_x)
-        # outline.noutrefresh()
-        game_area.noutrefresh()
-
-        return game_area
-
-    def make_message_box(self):
-        outline = curses.newwin(3, self.cols, self.lines - 3 + self.beg_y, 0 + self.beg_x)
-        outline.box()
-        message_box = curses.newwin(1, self.cols - 4, self.lines - 2 + self.beg_y, 2 + self.beg_x)
-        outline.noutrefresh()
-        message_box.noutrefresh()
-
-        return message_box
-
-    def main_loop(self):
-        while self.connected:
-            message = self.server.receive()
-            if message == BAD:
-                self.disconnect()
-            # elif message == BG:
-            #     self.update()
-            else:
-                self.process(message)
-    
-    def update(self):
-        pass
-                              
-    def join_server(self):
-        success = True
         self.server = ClientSocket()
-        curses.echo() # to see user input
-        wprint(self.message_box, "Server IP: ")
-        host = self.message_box.getstr().decode()
-        wclear(self.message_box)
-        wprint(self.message_box, "Server PORT: ")
-        try:
-            port = int(self.message_box.getstr().decode())
-            self.connect(host, port)
-        except Exception as e:
-            wclear(self.message_box)
-            wprint(self.message_box, f"Invalid Server Address: {e}")
-            sleep(2)
-            wclear(self.message_box)
-            wprint(self.message_box, "Press any key to return.")
-            curses.noecho() # turn off see user input
-            self.message_box.getch()
-            wclear(self.message_box)
-            success = False
-        finally:
-            wclear(self.message_box)
-        return success
+        self.server_addr = ()
+        self.eventloop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.eventloop)
 
-    def connect(self, host: str, port: int):
-            self.server_addr = (host, port)
-            self.server.connect(self.server_addr)
-            self.connected = True
-            print("You are connected to the server.")
-
-    def disconnect(self):
+    def disconnect(self, e: str = "You have been disconnected"):
         self.server.close()
         self.connected = False
-        print("You have been disconnected from the server.")
+        add_to_log(e)
     
+    def connect(self):
+        self.eventloop.run_until_complete(self._connect())
+        if self.connected:
+            updating = Thread(target=self.update_connected)
+            receiving = Thread(target=self.receive_messages)
+            updating.start()
+            receiving.start()
+            updating.join()
+            receiving.join()
+                
+    async def _connect(self, timeout: int = 1):
+        try:
+            self.server = ClientSocket()
+            connecting = asyncio.to_thread(self.server.connect, self.server_addr)
+            await asyncio.wait_for(connecting, timeout)
+            self.connected = True
+        except asyncio.TimeoutError:
+            add_to_log("Timeout Error: Server may currently be closed.")
+        except Exception as e:
+            add_to_log(f"Invalid Server Address: {e}")
+
+    def update_connected(self):
+        while self.connected:
+            sleep(1)
+            rlist, _, _ = select.select([self.server], [], [], 0)
+            if rlist:
+                try:
+                    data = self.server.recv(1, MSG_PEEK)
+                    if not data:
+                        self.disconnect(f"A Network Error occured.")
+                except Exception as e:
+                    self.disconnect(f"Server Error: {e}")
+
+    def receive_messages(self):
+        while self.connected:
+            try:
+                message = self.server.crecv()
+                if not message or message == BAD:
+                    self.disconnect()
+                else:
+                    self.process(message)
+            except Exception as e:
+                add_to_log(e)
+
+    @abstractmethod
     def process(self, message: str):
-        if message == IN:
-            actual_message = self.server.receive()
-            wprint(self.message_box, actual_message)
-            response = self.message_box.getstr().decode()
-            self.server.give(response)
-            wclear(self.message_box)
-        elif message == CLEAR:
-            actual_message = self.server.receive()
-            wclear(self.game_area)
-            wprint(self.game_area, actual_message)
-        else:
-            wprint(self.game_area, message)
-            
-if __name__ == "__main__":
-    client = Client()
-    client.main_loop()
+        ...
--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 from .sockets import ServerSocket, ClientSocket
-from .utils import asynchronus
-from .consts import HOST, PORT,\
-      IN, OUT, BAD, GOOD
-from time import sleep
+from .consts import *
+from threading import Thread
+from socket import gethostbyname, gethostname
 from _socket import MSG_PEEK
 import select
+from random import randint
+from time import asctime, sleep
+
+LOG_PATH = "server_debug_log.txt"
+ID = randint(1, 100)
+def add_to_log(message: str):
+    with open(LOG_PATH, 'a') as log:
+        log.write(f"{asctime()} | PID {ID}: {message}\n")
 
 class Host:
     def __init__(self, sock: ClientSocket, addr: tuple[str, int]):
@@ -25,9 +32,8 @@ class Host:
         end = "\n" if flag == OUT else ""
         message = message if isinstance(message, str) else str(message)
         if not self.connected:
-            # print(f"This host is no longer connected.")
-            ...
-        elif self.sock.give(message + end, flag) == BAD:
+            add_to_log(f"Server tried to send to {self.ip}, but this host is no longer connected.")
+        elif self.sock.csend(message + end, flag) == BAD:
             self.disconnect()
         else:
             ret = GOOD
@@ -37,9 +43,9 @@ class Host:
         '''Called when expecting any type of response from host.'''
         message = BAD
         if not self.connected:
-            print("This host is no longer connected.")
+            add_to_log(f"Server tried to receive from {self.ip}, but this host is no longer connected.")
         else:
-            message = self.sock.receive()
+            message = self.sock.crecv()
             if message == BAD:
                 self.disconnect()
         return message
@@ -47,13 +53,14 @@ class Host:
     def disconnect(self):
         self.connected = False
         self.sock.close()
-        print(f"{self.name} has disconnected")
+        if self.name:
+            print(f"{self.name} has disconnected")
     
     def connnect(self):
         self.connected = True
         print(f"{self.name} has connected.")
 
-    def is_connected(self):
+    def update_connected(self):
         rlist, _, _ = select.select([self.sock], [], [], 0)
         if rlist:
             try:
@@ -74,11 +81,15 @@ class Host:
 
 class Server:
     '''NOTE - .open must be called for the server to accecpt clients'''
-    def __init__(self, serv_addr: tuple[str, int] = (HOST, PORT), max_conn: int = 1):
+    def __init__(self, serv_addr: tuple[str, int] = (), max_conn: int = 1):
         '''
         Args:
             serv_addr (tuple[str, int]): the public ip address and the socket of the server
             max_conn (int): the maximum connections allowed by the server'''
+        if not serv_addr:
+            ip = gethostbyname(gethostname())
+            serv_addr = (ip, 5555)
+        add_to_log(f"Server Address: {serv_addr}")
         self.server_socket = ServerSocket()
         # binds the server to the ip and port specified
         self.server_socket.bind(serv_addr)
@@ -86,28 +97,39 @@ class Server:
         self.server_socket.listen(max_conn)
         self.hosts: list[Host] = []
         self.accepting = False
-    
+        
+    def close(self):
+        print("Server is now closed")
+        self.accepting = False
+        
     def open(self):
         '''Must be called to allow clients to connect to a server.'''
         print("Server is now open.")
         self.accepting = True
-        self.accept_clients()
-        self.update_connected()
-    
-    @asynchronus
+        Thread(target=self.accept_clients).start()
+        Thread(target=self.update_connected).start()
+        
     def update_connected(self):
         while True:
             sleep(1)
             for host_index in range(len(self.hosts) - 1, -1, -1):
                 host = self.hosts[host_index]
-                if not host.is_connected():
+                if not host.update_connected():
                     self.hosts.pop(host_index)
                     host.disconnect()
 
-
-    def close(self):
-        print("Server is now closed")
-        self.accepting = False
+    def accept_clients(self):
+        while True:
+            host_socket, host_addr = self.server_socket.accept()
+            if self.accepting:
+                ip = host_addr[0]
+                if ip in self.get_host_ips(): # put 'and False if you do not want unique connections'
+                    host = self.get_host_by_ip(ip)
+                    host.sock = host_socket # update to most current socket
+                    host.connnect()
+                    host.give(f"Welcome back {host.name}!")
+                else:
+                    self.setup_host(host_socket, host_addr)
 
     def broadcast(self, message: str | object, flag: str = OUT):
         '''Sends a message to all hosts currently connected to the server.
@@ -115,36 +137,13 @@ class Server:
         for host in self.hosts:
             if host.connected:
                 host.give(message, flag)
-
-    @asynchronus
-    def accept_clients(self):
-        while True:
-            host_socket, host_addr = self.server_socket.accept()
-            if self.accepting:
-                ip = host_addr[0]
-                if ip in self._get_host_ips(): # put 'and False if you do not want unique connections'
-                    host = self._get_host_by_ip(ip)
-                    host.sock = host_socket # update to most current socket
-                    host.connnect()
-                    host.give(f"Welcome back {host.name}!")
-                else:
-                    self.setup_host(host_socket, host_addr)
-    
-    # @background_process
-    # def broadcast_connected(self):
-    #     while True:
-    #         sleep(5)
-    #         connected_hosts = ""
-    #         for host in self.get_connected_hosts():
-    #             connected_hosts += host.name + " "
-    #         self.broadcast(connected_hosts.rstrip(), )
-
+                
     def setup_host(self, host_socket: ClientSocket, host_addr: tuple[str, int]):
         host = Host(host_socket, host_addr)
-        while host.set_name() in self._get_host_names():
-            host.sock.give("That name is taken!")
+        while host.set_name() in self.get_host_names():
+            host.sock.csend("That name is taken!")
         if host.name == BAD:
-            print("A Host failed to connect")
+            add_to_log(f"A Host {host.ip} failed to connect")
         else:
             host.connnect()
             self.hosts.append(host)
@@ -153,18 +152,14 @@ class Server:
     def get_connected_hosts(self):
         return [host for host in self.hosts if host.connected]
     
-    def _get_host_names(self):
+    def get_host_names(self):
         return [host.name for host in self.hosts]
 
-    def _get_host_ips(self) -> list[str]:
+    def get_host_ips(self) -> list[str]:
         return [host.ip for host in self.hosts]
     
-    def _get_host_by_ip(self, ip: str):
+    def get_host_by_ip(self, ip: str):
         for host in self.hosts:
             if host.ip == ip:
                 return host
         raise IndexError(f"No Host found by IP: {ip}")
-
-if __name__ == "__main__":
-    server = Server()
-    server.open()
